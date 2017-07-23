92991f7247e21c610e6002a2c155e95fd55388d2
Josh <josh@exasrc.com>
2017-07-23 20:56:53 +0100

    New static cache facade

--- src/StaticFacade.php
+++ src/StaticFacade.php
@@ -8,9 +8,18 @@ namespace J0sh0nat0r\SimpleCache;
 /**
  * A static wrapper around a SimpleCache instance (e.g for a global cache)
  *
- * @category Class
- * @author   Josh P
- * @package  J0sh0nat0r\SimpleCache
+ * @method  static bool|array  store(string|array $key, mixed $value, int $time = null)
+ * @method  static mixed       remember(string|array $key, int $time, \Closure $generate, mixed $default = null)
+ * @method  static bool|array  forever(string|array $key, mixed $value = null)
+ * @method  static bool|array  has(string|array $key)
+ * @method  static mixed       get(string|array $key, mixed $default)
+ * @method  static mixed       pull(string|array $key, mixed $default)
+ * @method  static bool|array  remove(string|array $key)
+ * @method  static bool        clear()
+ *
+ * @category  Class
+ * @author    Josh P
+ * @package   J0sh0nat0r\SimpleCache
  */
 class StaticFacade
 {
@@ -21,122 +30,36 @@ class StaticFacade
      */
     private static $cache;
 
+
+    /**
+     * Handle static calls and proxy them to $cache
+     *
+     * @param  string     $name       Name of the function being called
+     * @param  array|null $arguments  Arguments passed ot the function being called
+     *
+     * @return mixed
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        self::_checkBound();
+
+        if(method_exists(self::$cache, $name))
+        {
+            return call_user_func_array([self::$cache, $name], $arguments);
+        }
+
+        return null;
+    }
+
+
     /**
      * Bind the StaticFacade to a SimpleCache instance
      *
-     * @param mixed $cache
+     * @param  Cache $cache  The SimpleCache instance to bind to
      */
-    public static function setCache($cache)
+    public static function bind($cache)
     {
         self::$cache = $cache;
-    }
-
-    /**
-     * Store an item in the cache indefinitely
-     *
-     * @param $key
-     * @param null $value
-     *
-     * @return bool
-     */
-    public static function forever($key, $value = null)
-    {
-        self::_checkBound();
-
-        return self::$cache->forever($key, $value);
-    }
-
-    /**
-     * Store a value (or an array of key-value pairs) in the cache
-     *
-     * @param $key
-     * @param null $value
-     * @param int  $time
-     * 
-     * @return bool|array
-     * @throws \Exception
-     */
-    public static function store($key, $value = null, $time = null)
-    {
-        self::_checkBound();
-
-        return self::$cache->store($key, $value, $time);
-    }
-
-    /**
-     * Try to find a value in the cache and return it,
-     * if we can't it will be calculated with the provided closure
-     *
-     * @param string   $key      Key of the item to remember
-     * @param int      $time     Time to remember the item for (seconds)
-     * @param \Closure $generate Function used to generate the value to remember
-     * @param mixed    $default  Default return value (if item not found & generate returns null)
-     *
-     * @return mixed
-     */
-    public static function remember($key, $time, $generate, $default = null)
-    {
-        self::_checkBound();
-
-        return self::$cache->remember($key, $time, $generate, $default);
-    }
-
-    /**
-     * Fetch a value (or an multiple values) from the cache
-     *
-     * @param string $key     Key(s) of the items to fetch
-     * @param null   $default Default return value (if item not found)
-     *
-     * @return mixed
-     */
-    public static function get($key, $default = null)
-    {
-        self::_checkBound();
-
-        return self::$cache->get($key, $default);
-    }
-
-    /**
-     * Check if the cache contains an item
-     *
-     * @param string|array $key The key (or keys) to search for
-     *
-     * @return bool|array
-     */
-    public static function has($key)
-    {
-        self::_checkBound();
-
-        return self::$cache->has($key);
-    }
-
-    /**
-     * Fetch a value (or multiple values), remove it from the cache and then return it
-     *
-     * @param string|array $key     Key(s) of the item(s) to pull
-     * @param null         $default Default return value (if item not found)
-     *
-     * @return mixed
-     */
-    public static function pull($key, $default = null)
-    {
-        self::_checkBound();
-
-        return self::$cache->pull($key, $default);
-    }
-
-    /**
-     * Remove a value (or multiple values) from the cache
-     *
-     * @param string|array $key Key(s) of the item(s) to remove
-     *
-     * @return bool|array
-     */
-    public static function remove($key)
-    {
-        self::_checkBound();
-
-        return self::$cache->remove($key);
     }
 
 
