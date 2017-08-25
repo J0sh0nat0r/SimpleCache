ef32aa88766eb79f844e0b4e8dd345b7c476d0f1
Josh <josh@exasrc.com>
2017-08-25 23:27:48 +0100

    Vastly improved

--- src/Drivers/File.php
+++ src/Drivers/File.php
@@ -12,7 +12,7 @@ use J0sh0nat0r\SimpleCache\IDriver;
  * File driver.
  *
  * Accepted options:
- * dir - The directory to store cache files in
+ * dir -            The directory to store cache files in
  * encryption_key - The key to use for encrypting data, if not set data will NOT be encrypted
  */
 class File implements IDriver
@@ -24,7 +24,7 @@ class File implements IDriver
     public function __construct($options)
     {
         if (!isset($options['dir'])) {
-            throw new DriverOptionsInvalidException('No dir option passed for SimpleCache File driver');
+            throw new DriverOptionsInvalidException('Missing option: dir');
         }
 
         $this->dir = rtrim($options['dir'], '/');
@@ -37,103 +37,169 @@ class File implements IDriver
             $this->encryption_key = $options['encryption_key'];
         }
 
-        foreach (glob($this->dir.'/*', GLOB_ONLYDIR) as $item) {
+        $this->forAll(function ($item) {
             $data = json_decode(file_get_contents($item.'/data.json'), true);
 
             if ($data['expiry'] > 0 && time() >= $data['expiry']) {
                 $this->remove($data['key']);
             }
-        }
+        });
     }
 
     public function set($key, $value, $time)
     {
+        $dir = $this->getDir($key);
+
+        if (!is_dir($dir)) {
+            mkdir($dir);
+        }
+
+        $success = true;
         try {
-            $dir = $this->dir.'/'.sha1($key);
+            $expiry = $time > 0 ? time() + $time : null;
 
-            if (!is_dir($dir)) {
-                mkdir($dir);
-            }
-
-            $expiry = $time > 0 ? time() + $time : 0;
-
-            $item = [
+            $item_data = [
                 'key'       => $key,
                 'expiry'    => $expiry,
-                'encrypted' => $this->encrypt_data,
+                'encrypted' => $this->encrypt_data
             ];
 
             if ($this->encrypt_data) {
                 $value = $this->encrypt($value, $iv);
-                $item['iv'] = $iv;
+                $item_data['iv'] = $iv;
             }
 
-            file_put_contents($dir.'/data.json', json_encode($item));
-            file_put_contents($dir.'/item.dat', $value);
+            $success = file_put_contents($dir.'/data.json', json_encode($item_data)) ? $success : false;
+            $success = file_put_contents($dir.'/item.dat', $value) ? $success : false;
         } catch (\Exception $e) {
-            return false;
+            $success = false;
         }
 
-        return true;
+        return $success;
     }
 
     public function has($key)
     {
-        return is_dir($this->dir.'/'.sha1($key));
-    }
+        $data = $this->getData($key);
 
-    public function get($key)
-    {
-        $dir = $this->dir.'/'.sha1($key);
+        if (is_null($data)) {
+            return false;
+        }
 
-        if (is_dir($dir)) {
-            $data = json_decode(file_get_contents($dir.'/data.json'), true);
-
+        if (!is_null($data['expiry'])) {
             if ($data['expiry'] <= time()) {
                 $this->remove($key);
 
-                return null;
-            }
-
-            if ($this->encrypt_data) {
-                if ($data['encrypted']) {
-                    return $this->decrypt(file_get_contents($dir.'/item.dat'), $data['iv']);
-                }
-            }
-
-            return file_get_contents($dir.'/item.dat');
-        }
-
-        return null;
-    }
-
-    public function remove($key)
-    {
-        try {
-            $dir = $this->dir.'/'.sha1($key);
-
-            if (!is_dir($dir)) {
                 return false;
             }
-
-            unlink($dir.'/data.json');
-            unlink($dir.'/item.dat');
-            rmdir($dir);
-        } catch (\Exception $e) {
-            return false;
         }
 
         return true;
     }
 
+    public function get($key)
+    {
+        if (!$this->has($key)) {
+            return null;
+        }
+
+        $data = $this->getData($key);
+        $value = file_get_contents($this->getDir($key).'/item.dat');
+
+        if ($value === false) {
+            return null;
+        }
+
+        if ($data['encrypted']) {
+            if ($this->encrypt_data) {
+                throw new \Exception('Item is encrypted but no encryption key was provided');
+            }
+
+            $value = $this->decrypt($value, $data['iv']);
+        }
+
+        return $value;
+    }
+
+    public function remove($key)
+    {
+        if (!$this->has($key)) {
+            return true;
+        }
+
+        $dir = $this->getDir($key);
+
+        $success = true;
+        try {
+            $success = unlink($dir.'/data.json') ? $success : false;
+            $success = unlink($dir.'/item.dat') ? $success : false;
+            $success = rmdir($dir) ? $success : false;
+        } catch (\Exception $e) {
+            $success = false;
+        }
+
+        return $success;
+    }
+
     public function clear()
     {
-        foreach (glob($this->dir.'/*', GLOB_ONLYDIR) as $item) {
+        $this->forAll(function ($item) {
             $data = json_decode(file_get_contents($item.'/data.json'), true);
+
             $this->remove($data['key']);
+        });
+    }
+
+    /**
+     * Generates a directory based on an item's key
+     *
+     * @param string $key Key of the item to generate a directory for
+     *
+     * @return string
+     */
+    private function getDir($key)
+    {
+        return $this->dir.'/'.sha1($key);
+    }
+
+    /**
+     * Retrieves an item's data from disk
+     *
+     * @param string $key Key of the item who's data we're retrieving
+     *
+     * @return array|null
+     */
+    private function getData($key)
+    {
+        $dir = $this->getDir($key);
+
+        if (!is_dir($dir)) {
+            return null;
+        }
+
+        return json_decode(file_get_contents($dir.'/data.json'), true);
+    }
+
+    /**
+     * Calls callback on each item in the cache
+     *
+     * @param \Closure $callback Callback
+     */
+    private function forAll($callback)
+    {
+        foreach (glob($this->dir.'/*', GLOB_ONLYDIR) as $item) {
+            $callback($item);
         }
     }
 
+    /**
+     * Encrypts a string with the encryption key and provided initialisation vector
+     *
+     * @param string $data String to encrypt
+     * @param string $iv Encryption initialization vector
+     *
+     * @return string
+     */
     private function encrypt($data, &$iv)
     {
         $iv = bin2hex(openssl_random_pseudo_bytes(6));
@@ -141,6 +207,14 @@ class File implements IDriver
         return openssl_encrypt($data, 'aes-256-gcm', $this->encryption_key, 0, $iv);
     }
 
+    /**
+     * Decrypts a string with the encryption key and provided initialisation vector
+     *
+     * @param string $data String to decrypt
+     * @param string $iv The initialisation vector used to encrypt the item
+     *
+     * @return string
+     */
     private function decrypt($data, $iv)
     {
         return openssl_decrypt($data, 'aes-256-gcm', $this->encryption_key, 0, $iv);
