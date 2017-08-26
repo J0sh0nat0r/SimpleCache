0c965a91ded4eeba68dba87d6cb21496b6e92b19
Josh <josh@exasrc.com>
2017-08-26 02:59:50 +0100

    Enable APC for cli

--- tests/APCDriverTest.php
+++ tests/APCDriverTest.php
@@ -9,6 +9,11 @@ use J0sh0nat0r\SimpleCache\Drivers\APC as APCDriver;
 
 class APCDriverTest extends DriverTestCase
 {
+    public static function setUpBeforeClass()
+    {
+        ini_set('apc.enable_cli', true);
+    }
+    
     public function setUp()
     {
         $this->driver = new APCDriver();
