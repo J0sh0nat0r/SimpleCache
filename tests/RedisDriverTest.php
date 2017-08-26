b65752d5b0680322a95f22b1844c391d1c2d2345
Josh <josh@exasrc.com>
2017-08-26 22:29:41 +0100

    Test redis driver

---
+++ tests/RedisDriverTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright (c) 2017 Josh P (joshp.xyz).
+ */
+
+namespace J0sh0nat0r\SimpleCache\Tests;
+
+use J0sh0nat0r\SimpleCache\Drivers\Redis as RedisDriver;
+
+class RedisDriverTest extends DriverTestCase
+{
+    public function setUp()
+    {
+        $this->driver = new RedisDriver([
+            'host' => 'localhost',
+        ]);
+    }
+}
